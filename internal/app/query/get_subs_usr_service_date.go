1f9b34c5f2d8f43252a239df3f2f10b4195e40ab
Rikanskey <riko3144@gmail.com>
2025-10-23 18:36:28 +0300

    Fixed the calculation of the amount of subscriptions for the period (moved to the database and fixed errors)

--- internal/app/query/get_subs_usr_service_date.go
+++ internal/app/query/get_subs_usr_service_date.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"time"
 	"user-subscriptions/internal/app"
-	"user-subscriptions/internal/domain/subs"
 )
 
 type SubsFilterParams struct {
@@ -18,7 +17,7 @@ type getSubsByFilterReadModel interface {
 	GetSubsByUserIdServiceNameStarDateEndDate(
 		ctx context.Context,
 		sfp SubsFilterParams,
-	) ([]app.UserSubscription, error)
+	) (uint, error)
 }
 
 type GetSubsUserServiceDate struct {
@@ -30,30 +29,10 @@ func NewGetSubsUserServiceDate(readModel getSubsByFilterReadModel) GetSubsUserSe
 }
 
 func (h GetSubsUserServiceDate) Handle(ctx context.Context, qry app.GetSubByUserServiceDateQuery) (uint, error) {
-	subsDb, err := h.readModel.GetSubsByUserIdServiceNameStarDateEndDate(ctx, SubsFilterParams{
+	return h.readModel.GetSubsByUserIdServiceNameStarDateEndDate(ctx, SubsFilterParams{
 		UserId:    qry.UserId,
 		Service:   qry.Service,
 		StartDate: qry.StartDate,
 		EndDate:   qry.EndDate,
 	})
-	if err != nil {
-		return 0, err
-	}
-
-	unmarshallingParams := make([]subs.UnmarshallingParams, len(subsDb))
-
-	for _, s := range subsDb {
-		unmarshallingParams = append(unmarshallingParams, subs.UnmarshallingParams{
-			Id:        s.Id,
-			UserId:    s.UserId,
-			Service:   s.Service,
-			Price:     s.Price,
-			StartDate: s.StartDate,
-			EndDate:   s.EndDate,
-		})
-	}
-
-	userSubscriptions := subs.UnmarshalFromDatabaseCollection(unmarshallingParams)
-
-	return userSubscriptions.SumPrice(), nil
 }
